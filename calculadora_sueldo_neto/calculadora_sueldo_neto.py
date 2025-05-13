796e6d0f8b35db6d6fd4b004871a032ce0b3f12b
YandellCM <yandellcuevas94@gmail.com>
2025-05-12 20:05:27 -0400

    Proyecto Reflex listo: logica, entorno virtual y estructura

---
+++ calculadora_sueldo_neto/calculadora_sueldo_neto.py
@@ -0,0 +1,48 @@
+import reflex as rx  # Librería principal
+
+# Porcentajes  
+tss = 0.0591  # Seguridad Social
+isr = 0.15    # Impuesto Sobre la Renta
+bono = 0.10   # Bonificación
+
+# Estado de la aplicación tales como datos y lógica 
+class Estado(rx.State):
+    sueldo = 0.0
+    otros = 0.0
+    con_bono = False
+
+    descuento_seg = 0.0
+    descuento_isr = 0.0
+    bonificacion = 0.0
+    sueldo_neto = 0.0
+
+    def calcular(self):
+        self.descuento_seg = self.sueldo * tss
+        self.descuento_isr = self.sueldo * isr if self.sueldo > 34685 else 0
+        self.bonificacion = self.sueldo * bono if self.con_bono else 0
+        self.sueldo_neto = (
+            self.sueldo - self.descuento_seg - self.descuento_isr - self.otros + self.bonificacion
+        )
+
+# Interfaz web
+def vista():
+    return rx.center(
+        rx.vstack(
+            rx.heading("Calculadora de Sueldo Neto", size="lg"),
+            rx.input(placeholder="Escribe tu sueldo bruto", on_change=Estado.set_sueldo),
+            rx.input(placeholder="Escribe otros descuentos", on_change=Estado.set_otros),
+            rx.checkbox("¿La empresa da bonificación?", on_change=Estado.set_con_bono),
+            rx.button("Calcular", on_click=Estado.calcular),
+            rx.divider(),
+            rx.text("Descuento Seguridad Social: ", rx.var(Estado.descuento_seg)),
+            rx.text("Descuento ISR: ", rx.var(Estado.descuento_isr)),
+            rx.text("Bonificación: ", rx.var(Estado.bonificacion)),
+            rx.text("Sueldo Neto: ", rx.var(Estado.sueldo_neto)),
+            spacing="2em"
+        ),
+        padding="2em"
+    )
+
+# Iniciar la app
+app = rx.App()
+app.add_page(vista)
